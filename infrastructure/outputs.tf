73d017a391d8541c30e38f0bec9180ef5d9f5b0f
Luca CORRIERI <lucac@padok.fr>
2023-03-10 18:01:44 +0100

    fix(infra): add terraform output for private IP

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,3 +2,8 @@ output "openstack_public_ips" {
   value       = aws_instance.openstack[*].public_ip
   description = "IP of the public instance"
 }
+
+output "openstack_private_ips" {
+  value       = aws_instance.openstack[*].private_ip
+  description = "Private IPs of the OpenStack instance"
+}
